23e873ff13e82321ddbbdab2d9162298d394863f
marianahermidarojas <mariana.02.hr@gmail.com>
2022-10-10 21:12:20 -0500

    Finished app, changed compileSdk from 30 to 32

--- app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
+++ app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
@@ -15,24 +15,51 @@
  */
 package com.example.amphibians.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.amphibians.network.Amphibian
+import com.example.amphibians.network.AmphibianAPI
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 enum class AmphibianApiStatus {LOADING, ERROR, DONE}
 
 class AmphibianViewModel : ViewModel() {
 
-    // TODO: Create properties to represent MutableLiveData and LiveData for the API status
+    // Properties to represent MutableLiveData and LiveData for the API status
+    private val _status = MutableLiveData<AmphibianApiStatus>()
+    val status: LiveData<AmphibianApiStatus>
+        get() = _status
 
-    // TODO: Create properties to represent MutableLiveData and LiveData for a list of amphibian objects
+    // Properties to represent MutableLiveData and LiveData for a list of amphibian objects
+    private val _amphibians = MutableLiveData<List<Amphibian>>()
+    val amphibians: LiveData<List<Amphibian>>
+        get() = _amphibians
 
-    // TODO: Create properties to represent MutableLiveData and LiveData for a single amphibian object.
-    //  This will be used to display the details of an amphibian when a list item is clicked
+    // Properties to represent MutableLiveData and LiveData for a single amphibian object.
+    private val _amphibian = MutableLiveData<Amphibian>()
+    val amphibian: LiveData<Amphibian>
+        get() = _amphibian
 
-    // TODO: Create a function that gets a list of amphibians from the api service and sets the
-    //  status via a Coroutine
+    // Function that gets a list of amphibians from the api service and sets the status via
+    // a Coroutine
+    fun getAmphibianList(){
+        viewModelScope.launch{
+            _status.value = AmphibianApiStatus.LOADING
+            try {
+                _amphibians.value = AmphibianAPI.retrofitService.getAmphibians()
+                _status.value = AmphibianApiStatus.DONE
+            } catch (e: Exception){
+                _amphibians.value = emptyList()
+                _status.value = AmphibianApiStatus.ERROR
+            }
+        }
+    }
 
+    // Function to set amphibian to amphibian passed as argument
     fun onAmphibianClicked(amphibian: Amphibian) {
-        // TODO: Set the amphibian object
+        _amphibian.value = amphibian
     }
 }
